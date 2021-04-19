532eaa241e43495267caf39d4e6bb45ae76ff082
anchal77singh <anchal77singh@gmail.com>
2021-04-19 16:51:54 +0530

    my first commit

---
+++ core-java-project/src/com/sapient/trg/pl/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.sapient.trg.pl;
+
+import java.util.Date;
+
+/**
+ * This is java doc comments
+ * @author ancsingh2
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+		System.out.println("Today's Date: "+new Date());
+	}
+
+}
